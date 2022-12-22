09712601bdc99f24829ec1181ef22ce12f3f1c08
junior mojica <juniormojica26@gmail.com>
2022-12-22 16:16:01 -0500

    routing integration Complete

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../Detail/Detail.module.css"
 
 const Detail = ()=>{
     const{ detailId }= useParams()
     const [character,setCharacter]=useState({})
+    const navigation = useNavigate()
     
     useEffect(()=>{
         fetch(`https://rickandmortyapi.com/api/character/${detailId}`)
@@ -24,16 +26,22 @@ const Detail = ()=>{
     
     console.log(character);
     return(
-        <div>
-            <div>
-                <h1 className= {styles.info}>{character.name}</h1>
-                <h3 className= {styles.info}>{character.status} </h3>
-                <h3 className= {styles.info}>{character.specie} </h3>
-                <h3 className= {styles.info}>{character.gender} </h3>
+        <div >
+          
+            <div className={styles.detailContainer}>
+                <div>
+                    <h1 className= {styles.info}>{`Nombre: ${character.name}`}</h1>
+                    <h3 className= {styles.info}>{`Status: ${character.status}`} </h3>
+                    <h3 className= {styles.info}>{`Specie: ${character.species}`} </h3>
+                    <h3 className= {styles.info}>{`Gender: ${character.gender}`} </h3>
+                    <h3 className={styles.info}> {`Origen :${character.origin && character.origin.name ? character.origin.name : "No hay nombre de origen"}`} </h3>
+                </div>
+                <div>
+                      <h1><img src={character.image} alt="" srcset="" /></h1>
+                </div>
                 
             </div>
-            <div></div>
-            <h1><img src={character.image} alt="" srcset="" /></h1>
+            <button onClick={()=>navigation("/")}>Volver</button>
         </div>
     )
 }
